b868bbf40f37e3afcc343340186df1658a15826b
Mariusz Wojtach <mariusz.wojtach@gmail.com>
2013-03-14 16:13:06 +0100

    Customers Index&Add

---
+++ WarehouseAgile/Models/CustomersModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WarehouseAgile.Models
+{
+    public class CustomersModel
+    {
+        private List<Customer> customers;
+
+        public List<Customer> Customers
+        {
+            get
+            {
+                return customers;
+            }
+        }
+
+        public void Index()
+        {
+            customers = new List<Customer>();
+            using (AppDBEntities db = new AppDBEntities())
+            {
+                foreach (Customer c in db.Customers)
+                {
+                    customers.Add(c);
+                }
+            }
+        }
+    }
+}
